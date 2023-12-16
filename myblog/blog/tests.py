2eb81b22fe09e3f217da2cfcec828147f224ec70
Sohaib <sohaib.uet12@gmail.com>
2023-12-16 22:56:54 +0500

    implemented the HomePage tests

--- myblog/blog/tests.py
+++ myblog/blog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Entry
+from django.contrib.auth import get_user_model
 
 
 # Create your tests here.
@@ -18,3 +19,28 @@ class ProjectTest(TestCase):
     def test_home_page(self):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
+
+
+class HomePageTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create(username='test_user')
+
+    def test_one_entry(self):
+        Entry.objects.create(title='1-title', body='1-body', author=self.user)
+        response = self.client.get('/')
+        self.assertContains(response, '1-title')
+        self.assertContains(response, '1-body')
+
+    def test_two_entires(self):
+        Entry.objects.create(title='1-title', body='1-body', author=self.user)
+        Entry.objects.create(title='2-title', body='2-body', author=self.user)
+        response = self.client.get('/')
+        self.assertContains(response, '1-title')
+        self.assertContains(response, '2-title')
+        self.assertContains(response, '1-body')
+        self.assertContains(response, '2-body')
+
+    def test_no_entries(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'No blog entries yet.')
